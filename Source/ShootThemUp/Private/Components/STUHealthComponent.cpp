4105c4f5263533f49c15de33befc7c9b812d6649
Ullanar <lev.boubnov@gmail.com>
2022-01-14 01:30:59 +0300

    health timer

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -1,40 +1,57 @@
 // YWA-Interactive. All Rights Reserved.
 
-
 #include "Components/STUHealthComponent.h"
-#include "GameFramework/Actor.h"
 
-DEFINE_LOG_CATEGORY_STATIC(DamageDevSphere, All, All)
+#include "Engine/World.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
 
 // Sets default values for this component's properties
 USTUHealthComponent::USTUHealthComponent() {
   PrimaryComponentTick.bCanEverTick = false;
 }
 
-
 // Called when the game starts
 void USTUHealthComponent::BeginPlay() {
   Super::BeginPlay();
-  Health = MaxHealth;
+  SetHealth(MaxHealth);
 
-  OnHealthChanged.Broadcast(Health);
-  
   AActor* ComponentOwner = GetOwner();
 
-  if (ComponentOwner) ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::HandleTakeAnyDamage);
-  
+  if (ComponentOwner)
+    ComponentOwner->OnTakeAnyDamage.AddDynamic(
+        this, &USTUHealthComponent::HandleTakeAnyDamage);
 }
 
-void USTUHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor, float Damage,
-    const UDamageType* DamageType, AController* InstigatedBy,
-    AActor* DamageCauser) {
-  
-  if (Damage <= 0 || IsDead()) return;
+void USTUHealthComponent::HealUpdate() {
+  SetHealth(Health + HealModifier);
 
-  Health = FMath::Clamp(Health - Damage, 0.0f, MaxHealth);
+  if (FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld()) {
+    GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+  }
+}
+
+void USTUHealthComponent::SetHealth(float NewHealth) {
+  Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
   OnHealthChanged.Broadcast(Health);
+}
+
+void USTUHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor,
+                                              float Damage,
+                                              const UDamageType* DamageType,
+                                              AController* InstigatedBy,
+                                              AActor* DamageCauser) {
+  if (Damage <= 0 || IsDead() || !GetWorld()) return;
+
+  SetHealth(Health - Damage);
+
+  GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
 
   if (IsDead()) {
-   OnDeath.Broadcast(); 
+    OnDeath.Broadcast();
+  } else if (IsAutoHealEnabled) {
+    GetWorld()->GetTimerManager().SetTimer(
+        HealTimerHandle, this, &USTUHealthComponent::HealUpdate, HealUpdateTime,
+        true, HealStartDelayAfterGetDamage);
   }
 }
--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -29,12 +29,29 @@ protected:
   UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Health", meta=(ClampMin = "0", ClampMax="1000"))
   float MaxHealth = 100;
 
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Heal");
+  bool IsAutoHealEnabled = true;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Heal", meta=(EditCondition = "AutoHeal"));
+  float HealUpdateTime = 1.0f;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Heal", meta=(EditCondition = "AutoHeal"));
+  float HealStartDelayAfterGetDamage = 3.0f;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Heal", meta=(EditCondition = "AutoHeal"));
+  float HealModifier = 5.0f;
+
 private:
-  float Health = 0;
+  float Health = 0.0f;
+  FTimerHandle HealTimerHandle;
+
+  void HealUpdate();
+
+  void SetHealth(float NewHealth);
 
   UFUNCTION()
-void HandleTakeAnyDamage(AActor* DamagedActor, float Damage,
-                         const class UDamageType* DamageType,
-                         class AController* InstigatedBy,
-                         AActor* DamageCauser);
+  void HandleTakeAnyDamage(AActor* DamagedActor, float Damage,
+                           const class UDamageType* DamageType,
+                           class AController* InstigatedBy,
+                           AActor* DamageCauser);
 };
